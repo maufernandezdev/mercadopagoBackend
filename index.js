806b043ec6ce844f3293b9acb89c04b65e0d8d36
Mauricio Fernandez <mfernandezuk@hotmail.com>
2022-11-09 20:38:00 -0300

    chenge routers to routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 9001;
-const routerCheckout = require('./routers/checkout');
+const routerCheckout = require('./routes/checkout');
 app.use('/api/checkout', routerCheckout);
 
 app.get('/', (req, res)=>{
