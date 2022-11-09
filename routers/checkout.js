957df86f4aa6dcddffd6f9f24e8593603e5cf49c
Mauricio Fernandez <mfernandezuk@hotmail.com>
2022-11-09 20:19:01 -0300

    testing mercadopago async function

--- routers/checkout.js
+++ routers/checkout.js
@@ -4,6 +4,20 @@ const mercadopago = require('mercadopago');
 mercadopago.configure({access_token: "TEST-7974656510754281-030814-20836cabe8f81d381a051d44e9a11510-162067010"}); // seller account credentials
 routerCheckout.use(express.json()); // Middleware
 
+const checkoutMercadopago = async (preference) =>
+{
+  try
+  {
+    const { response } = await mercadopago.preferences.create(preference);
+    const data = await response.json();
+    return JSON.stringify(data.body);
+  }
+  catch(error)
+  {
+    return error;
+  }
+}
+
 routerCheckout.post('/',(req , res) => {
 
     const preference = {
@@ -15,15 +29,16 @@ routerCheckout.post('/',(req , res) => {
           },
         ],
     };
-
-    mercadopago.preferences
-    .create(preference)
-    .then(function (response) {
-      return res.send(JSON.stringify(response.body));
-    })
-    .catch(function (error) {
-        res.send(error)
-    });
+    const resMercadopago = checkoutMercadopago(preference);
+    res.send(resMercadopago);
+    // mercadopago.preferences
+    // .create(preference)
+    // .then(function (response) {
+    //   return res.send(JSON.stringify(response.body));
+    // })
+    // .catch(function (error) {
+    //     console.log(error);
+    // });
 });
 
 module.exports = routerCheckout;
