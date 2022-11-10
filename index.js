5fbe858ef6fd8b5afcaa75929111809e397b6444
Mauricio Fernandez <mfernandezuk@hotmail.com>
2022-11-09 21:22:25 -0300

    rollback router in vercel json - adding app to routes

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 9001;
 const routerCheckout = require('./routes/checkout');
+const appRoute = require('./routes/app')
 app.use('/api/checkout', routerCheckout);
-
-app.get('/', (req, res)=>{
-    res.send("servidor para realizar checkout con mercadopago");
-})
+app.use('/', appRoute);
 
 app.listen(port,() => {
     console.log(`Listening to port ${port}`);
