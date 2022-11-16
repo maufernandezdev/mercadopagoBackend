f7733738681e6f81b167f99747a45705441a19c6
Mauricio Fernandez <mfernandezuk@hotmail.com>
2022-11-15 21:09:00 -0300

    adding cors and handle notification url

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 9001;
 const checkoutRoute = require('./routes/checkout');
 app.use('/api/checkout', checkoutRoute);
@@ -7,6 +9,10 @@ app.use('/api/checkout', checkoutRoute);
 app.get('/', (req, res)=>{
     return res.send('servidor para realizar checkout con mercadopago');
 });
+
+app.get('*', (req, res) => {
+    res.send('404 ruta no encontrada en el servidor');
+})
 
 app.listen(port,() => {
     console.log(`Listening to port ${port}`);
--- routes/checkout.js
+++ routes/checkout.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const checkoutRoute = express.Router();
-checkoutRoute.use(express.json()); // Middleware
-const mercadopago = require('mercadopago'); // SDK de Mercado Pago
+checkoutRoute.use(express.json()); 
+const mercadopago = require('mercadopago');
 mercadopago.configure({access_token: 'TEST-7974656510754281-030814-20836cabe8f81d381a051d44e9a11510-162067010'}); // seller account credentials
+
+checkoutRoute.get('/success', (req,res) =>{
+    console.log(req.query);
+    console.log(req.query.payment_id);
+    console.log(req.query.status);
+    // save data in a data base ad then redirect to ecommerce again
+    res.redirect(`https://mercadopago-frontend.vercel.app/success.html?payment_id=${req.query.payment_id}`);
+});
+
+checkoutRoute.get('/mercadopago/status', (req,res) =>{
+    //handle webhook from mercadopago - notification url
+    res.status(200).send(req.body);
+    return 'OK';
+});
 
 checkoutRoute.post('/',(req , res) => {
 
+    const status = `https://mercadopago-backend.vercel.app/api/checkout/mercadopago/status`
     const preference = {
         items: req.body,
         back_urls: {
@@ -14,11 +29,12 @@ checkoutRoute.post('/',(req , res) => {
             pending: 'https://mercadopago-backend.vercel.app/api/checkout/pending'
         },
         auto_return: 'approved',
+        notification_url: status,
     };
     mercadopago.preferences
     .create(preference)
     .then(function (response) {
-      return res.redirect(response.body.init_point);
+      return res.send({ init_point: response.body.init_point });
     })
     .catch(function (error) {
         res.send(error)
